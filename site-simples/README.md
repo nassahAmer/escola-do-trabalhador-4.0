b7c972ed96b5a6d848d452bbbda288c86999992f
nassahAmer <hassan.amer.rr@gmail.com>
2022-08-16 12:50:35 -0400

    Adição de lógica, utilizando javascript, para alternar o tema do site

--- site-simples/README.md
+++ site-simples/README.md
@@ -1,5 +1,6 @@
 # Criação de Site Simples
 ## Usando HTML5, CSS3 e javascript
 Página simples utilizando:
-* Tecnologia javascript para modificar o tema do site Claro ou Escuro
-* Tecnologia CSS3 para dar visual ao site
+* Tecnologia HTML5 para criar o 'esqueleto' do site
+* Tecnologia javascript para modificar o visual/tema do site para Claro ou Escuro
+* Tecnologia CSS3 para criar os visuais do site
--- site-simples/js/script.js
+++ site-simples/js/script.js
@@ -6,5 +6,23 @@
 */
 'use strict'
 
+// mudarTema recebe a referência do botão de classe .btn
+const mudarTema = document.querySelector('.btn');
 
+// No addEventListeneer o
+// Ouvinte é o 'click'
+// Manipulador é a função
+mudarTema.addEventListener('click', function(){
+    // toggle alterna a classe para 'dark-theme' automaticamente
+    document.body.classList.toggle('dark-theme');
+
+    // atualizar rótulo do botão
+    let className = document.body.className;
+    if(className == 'light-theme') {
+        this.textContent = "Escuro";
+    } else {
+        this.textContent = "Claro";
+    }
+
+});
 
